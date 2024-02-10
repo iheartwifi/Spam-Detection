ecaae5d4b0774c32d0bf5f807396f2c615643a1c
iheartwifi <wanlanhe@gmail.com>
2024-02-10 12:46:05 -0500

    Implementation attempt

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-# app.py
+#Dans cet exemple, app.py est le script principal de notre application web qui utilise Flask. 
+#Nous avons une route principale '/' qui rend un modèle de formulaire où l'utilisateur peut saisir du texte. 
+#Lorsque l'utilisateur soumet le formulaire, la route '/predict' est appelée, 
+#où nous utilisons le modèle chargé pour effectuer une prédiction et renvoyer le résultat à l'utilisateur.
+
+
 from flask import Flask, render_template, request
 import pickle
 
 app = Flask(__name__)
 
 # Charger le modèle au démarrage de l'application
-with open('Naive_model.pkl', 'rb') as model_file:
+with open('Naive_move.pkl', 'rb') as model_file:
     model = pickle.load(model_file)
 
 @app.route('/')
